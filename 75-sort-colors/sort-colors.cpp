332abacbb2ff4d112e99ca1d67b09eab1ccdf8fa
Mahfuzul Islam Pranto <121658967+MahfuzulIslamPranto@users.noreply.github.com>
2026-05-03 20:16:16 +0600

    Time: 0 ms (100.00%) | Memory: 11.5 MB (77.69%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -24,11 +24,11 @@ public:
         while (c3--) {
             nums.push_back(2);
         }
-        cout<<"[";
-        for(int i=0;i<nums.size();i++){
-            cout<<nums[i];
-            if(i != nums.size()-1) cout<<",";
-        }
-        cout<<"]";
+        // cout<<"[";
+        // for(int i=0;i<nums.size();i++){
+        //     cout<<nums[i];
+        //     if(i != nums.size()-1) cout<<",";
+        // }
+        // cout<<"]";
     }
 };
