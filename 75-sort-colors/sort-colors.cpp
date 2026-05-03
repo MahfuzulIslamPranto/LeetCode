6c465bee100085a4e6aebe2b8c4e994da780efbd
Mahfuzul Islam Pranto <121658967+MahfuzulIslamPranto@users.noreply.github.com>
2026-05-03 20:14:05 +0600

    Time: 4 ms (1.13%) | Memory: 11.7 MB (47.17%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> ans;
+        int c1 = 0;
+        int c2 = 0;
+        int c3 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                c1++;
+            } else if (nums[i] == 1) {
+                c2++;
+            } else {
+                c3++;
+            }
+        }
+        nums.clear();
+        while (c1--) {
+            nums.push_back(0);
+        }
+        while (c2--) {
+            nums.push_back(1);
+        }
+        while (c3--) {
+            nums.push_back(2);
+        }
+        cout<<"[";
+        for(int i=0;i<nums.size();i++){
+            cout<<nums[i];
+            if(i != nums.size()-1) cout<<",";
+        }
+        cout<<"]";
+    }
+};
