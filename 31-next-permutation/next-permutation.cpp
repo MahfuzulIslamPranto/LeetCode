ce1f7b12d98bcff83296b9d0e33e47329c2fe36f
Mahfuzul Islam Pranto <121658967+MahfuzulIslamPranto@users.noreply.github.com>
2026-05-05 10:21:49 +0600

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.17%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int sz = nums.size();
-        if(next_permutation(nums.begin(),nums.end())){
-            cout<<"[";
-            for(int i=0;i<sz;i++){
-                cout<<nums[i];
-                if(i != sz-1) cout<<",";
-            }
-            cout<<"]";
-        }
+        next_permutation(nums.begin(),nums.end());
+        // int sz = nums.size();
+        // // if(next_permutation(nums.begin(),nums.end())){
+        // //     cout<<"[";
+        // //     for(int i=0;i<sz;i++){
+        // //         cout<<nums[i];
+        // //         if(i != sz-1) cout<<",";
+        // //     }
+        // //     cout<<"]";
+        // // }
     }
 };
