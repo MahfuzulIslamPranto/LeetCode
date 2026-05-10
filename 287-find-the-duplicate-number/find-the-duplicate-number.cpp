67ddf68d76178942cad0b049cc5f5c5c26223b0b
Mahfuzul Islam Pranto <121658967+MahfuzulIslamPranto@users.noreply.github.com>
2026-05-10 13:01:58 +0600

    Time: 7 ms (50.66%) | Memory: 91.6 MB (7.86%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,10 +4,10 @@ public:
         int mx = *max_element(nums.begin(),nums.end());
         int mn = *min_element(nums.begin(),nums.end());
         vector<int> fre(100005,0);
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){//you are playing with indicies
             fre[nums[i]]++;
         }
-        for(int i=mn;i<=mx;i++){
+        for(int i=mn;i<=mx;i++){ // your're playing with elements. So you have to return elements which is i
             if(fre[i]>1){
                 return i;
             }
